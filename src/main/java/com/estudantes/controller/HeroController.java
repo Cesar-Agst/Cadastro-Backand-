0c47326eb2538a9773883f98f5a8dcf781670eb3
Cesar Augusto <79151738+Cesar-Agst@users.noreply.github.com>
2021-06-22 22:39:46 -0300

    id mudança

--- src/main/java/com/estudantes/controller/HeroController.java
+++ src/main/java/com/estudantes/controller/HeroController.java
@@ -1,15 +1,19 @@
 package com.estudantes.controller;
 
-
 import java.util.List;
+import java.util.Optional;
 
-import com.estudantes.Entities.Hero;
-import com.estudantes.repository.HeroRepository;
+import com.estudantes.entities.Estudantes;
+import com.estudantes.repository.EstudantesRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 
@@ -29,5 +33,14 @@ public class HeroController {
         
 
         return lista;
+    }
+    
+ @GetMapping("{id}")
+    public Hero getHero(@PathVariable Long id){
+
+        Optional<Hero> op = repo.findById(id);
+         Hero hero = op.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return hero;
     }
 }
